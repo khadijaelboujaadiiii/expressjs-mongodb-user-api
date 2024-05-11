0fcb2da9590a9133aae524283211e0fbde1d581a
elboujaadi <khadijaelboujaadi@gmail.com>
2024-05-11 11:17:36 +0100

    db db

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/my_database', {
+
+useNewUrlParser: true,
+
+useUnifiedTopology: true
+
+})
+
+.then(() => console.log('MongoDB connected...'))
+
+.catch(err => console.log(err));
